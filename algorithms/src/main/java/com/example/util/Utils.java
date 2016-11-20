8f53955630fe2fcbcfbfa3c3ef78eedea85ec24e
AckywOw <ackywow@163.com>
2016-11-20 16:42:21 +0800

    encode fix

---
+++ algorithms/src/main/java/com/example/util/Utils.java
@@ -0,0 +1,16 @@
+package com.example.util;
+
+import java.util.Arrays;
+
+/**
+ * Created by AckywOw on 2016/11/20.
+ */
+
+public class Utils {
+
+    public static void printArrs(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+}
