57885821bd7da044df639640f4e2025ca1583236
ackywow <ackywow@163.com>
2017-08-07 16:29:43 +0800

    增加带tag标题强布局控件 update

--- app/src/main/java/com/ackywow/session/view/TextViewAsTitleWithImagesLayout.java
+++ app/src/main/java/com/ackywow/session/view/TextViewAsTitleWithImagesLayout.java
@@ -1,11 +1,13 @@
 package com.ackywow.session.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import com.ackywow.session.R;
 
 /**
  * 用作带图片tag的标题布局，可以带多个图片tag，左右均可，右边tag可以一部分左对齐，一部分跟随标题文字
@@ -13,6 +15,8 @@ import android.widget.TextView;
  */
 
 public class TextViewAsTitleWithImagesLayout extends RelativeLayout {
+
+  private int target_id = NO_ID;
 
   public TextViewAsTitleWithImagesLayout(Context context) {
     this(context, null);
@@ -25,6 +29,20 @@ public class TextViewAsTitleWithImagesLayout extends RelativeLayout {
   public TextViewAsTitleWithImagesLayout(Context context, @Nullable AttributeSet attrs,
       int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+    TypedArray ta =
+        context.obtainStyledAttributes(attrs, R.styleable.TextViewAsTitleWithImagesLayout,
+            defStyleAttr, 0);
+    for (int i = 0, count = ta.getIndexCount(); i < count; i++) {
+      int attr = ta.getIndex(i);
+      switch (attr) {
+        case R.styleable.TextViewAsTitleWithImagesLayout_target:
+          target_id = ta.getResourceId(attr, NO_ID);
+          break;
+        default:
+          break;
+      }
+    }
+    ta.recycle();
   }
 
   @Override
@@ -35,15 +53,17 @@ public class TextViewAsTitleWithImagesLayout extends RelativeLayout {
     for (int i = 0; i < getChildCount(); i++) {
       View child = getChildAt(i);
       RelativeLayout.LayoutParams lp = (LayoutParams) child.getLayoutParams();
-      if (child instanceof TextView) {
+      if (child instanceof TextView && child.getId() == target_id) {
         tv = (TextView) child;
         childAllWidth -= lp.leftMargin;
-      } else {
+      } else if (child.getVisibility() != GONE) {
         childAllWidth -= child.getMeasuredWidth() + lp.leftMargin;
       }
     }
     if (tv != null && tv.getMeasuredWidth() > childAllWidth) {
-      tv.setWidth(childAllWidth);
+      tv.setMaxWidth(childAllWidth);
+    } else {
+      tv.setMaxWidth(Integer.MAX_VALUE);
     }
   }
 }
